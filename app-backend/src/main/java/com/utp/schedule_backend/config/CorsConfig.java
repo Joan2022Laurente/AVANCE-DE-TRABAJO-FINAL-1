c398ebb61d38c8b19c0aabcb22ba75574b52d197
TuNombreDeUsuario <TuCorreo@dominio.com>
2025-12-05 21:36:39 -0500

    sdfsfsdf

--- app-backend/src/main/java/com/utp/schedule_backend/config/CorsConfig.java
+++ app-backend/src/main/java/com/utp/schedule_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // tu frontend
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://utpschedule.vercel.app/") // tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
